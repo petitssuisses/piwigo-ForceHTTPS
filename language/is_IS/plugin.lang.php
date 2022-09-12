088d7441fa89b3736d6618ad9f3cfe4af0228de1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-12 06:27:21 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['fhp_force_https_comment'] = 'Virkjaðu þennan valkost til að þvinga viðskiptavini til að tengjast Piwigo í gegnum örugga tengingu.';
+$lang['fhp_force_https_label'] = 'Þvingaðu HTTPS';
+$lang['fhp_force_https_warning'] = '<b>Viðvörun!</b> Áður en þú virkjar örugga tengingu skaltu ganga úr skugga um að þjónninn þinn styðji https. Þú getur smellt á eftirfarandi hlekk til að prófa (hleður þessa síðu í https ham).';
+$lang['fhp_options_group'] = 'Valkostir';
+$lang['fhp_security_group'] = 'Öryggisstillingar';
+$lang['fhp_sts_maxage_comment'] = 'STS hámarksaldur í sekúndum';
+$lang['fhp_sts_maxage_label'] = 'STS hámarksaldur';
+$lang['fhp_submit'] = 'Senda';
+$lang['fhp_test_https_link'] = 'Prófaðu þessa síðu í https (sprettigluggi)';
+$lang['max-age must be set to a minimum of six months (15768000), but longer periods such as two years (63072000) are recommended'] = 'Hámarksaldur ætti að vera að lágmarki sex mánaða lengd (15768000), en mælt er með lengri tíma eins og tvö ár (63072000)';
+$lang['seconds'] = 'sekúndur';
+$lang['Force HTTPS for administration'] = 'Þvingaðu HTTPS fyrir stjórnun';
+$lang['Force HTTPS for identification'] = 'Þvingaðu HTTPS fyrir auðkenningu';
+$lang['Force usage of HTTPS on this site'] = 'Þvingaðu notkun á HTTPS fyrir þessa vefsíðu';
+$lang['Global HTTPS'] = 'Alhliða HTTPS';
+$lang['Global activation'] = 'Alhliða virkjun';
+$lang['HTTPS activated for administration pages only'] = 'HTTPS aðeins virkt fyrir stjórnunarsíður';
+$lang['HTTPS activated for identification, register and profile pages only'] = 'HTTPS aðeins virkt fyrir auðkenningu, skráningu og prófílsíður';
+$lang['HTTPS available'] = 'HTTPS í boði';
+$lang['HTTPS unavailable'] = 'HTTPS ekki í boði';
+$lang['I have proceeded to the manual check'] = 'Ég hef haldið áfram í handvirku athugunina';
+$lang['Manual Verification'] = 'Handvirk staðfesting';
+$lang['Parial activation'] = 'Virkjun að hluta';
+$lang['Plugin could not automatically verify your host SSL/HTTPS compatibility. Please manually check that HTTPS navigation is available using the following button. Do NOT activate HTTPS if page within the popup can not be loaded successfully.'] = 'Viðbótin gat ekki sjálfkrafa staðfest SSL/HTTPS samhæfni hýsingaraðila. Athugaðu handvirkt hvort HTTPS leiðarstýring sé tiltæk með því að nota eftirfarandi hnapp. EKKI virkja HTTPS ef ekki er hægt að hlaða síðu í sprettiglugganum.';
+$lang['Your hosting server does not support HTTPS, a SSL certificate is required. Plugin activation is disabled.'] = 'Hýsingarþjónninn þinn styður ekki HTTPS, SSL vottorðs er krafist. Virkjun viðbótar er óvirk.';
+$lang['Your hosting server does support HTTPS.'] = 'Hýsingarþjónninn þinn styður HTTPS.';
+$lang['fhp_about'] = 'Um';
+$lang['fhp_activate_sts_comment'] = 'Virkt HTTP Strict Transport Security (HSTS)<br/>Þessi valkostur verður að vera virkur ef þú átt undirritað TLS vottorð. Það hefur engin áhrif ef slökkt er á "Force HTTPS"<br/><br/> <i>HSTS er veföryggiskerfi þar sem vefþjónninn lýsir því yfir að vafrinn hafi samskipti við hann eingöngu með því að nota öruggar HTTPS tengingar. Þegar HSTS er virkt er hausreitur „Strict-Transport-Security“ settur í http haus og öllum veftenglum er sjálfkrafa skipt út fyrir öruggan hlekk.<br/></i>';
+$lang['fhp_activate_sts_comment_short'] = 'Virkjar HTTP Strict Transport Security (HSTS). Eingöngu til notkunar ef þú átt undirritað TLS vottorð';
+$lang['fhp_activate_sts_label'] = 'Nota STS';
+$lang['fhp_description'] = 'Þessi viðbót fyllir upp í öryggisgat í Piwigo. Það gerir stjórnendum kleift að þvinga fram notkun á SSL öruggum tengingum til að vafra um Piwigo (þegar https er tiltækt á hýsingarþjóninum). <br>Það er sérstaklega mælt með því að nota öruggar tengingar til að vernda sendingar notenda og lykilorða.';
+$lang['301 permanent, 302 temporary'] = '301 varanlegt, 302 tímabundið';
+$lang['Advanced options'] = 'Ítarlegir valkostir';
