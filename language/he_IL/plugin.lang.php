98bd4433bf95f0850a35592162b92b5a02bca5d3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:23:16 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['301 permanent, 302 temporary'] = '301 permanent, 302 temporary';
+$lang['Advanced options'] = 'אפשרויות מתקדמות';
+$lang['Force HTTPS for administration'] = 'לכפות HTTPS למנהל';
+$lang['Force HTTPS for identification'] = 'לכפות HTTPS לזיהוי';
+$lang['Force usage of HTTPS on this site'] = 'לכפות שימוש ב-HTTPS באתר זה';
+$lang['Global HTTPS'] = 'HTTPS גלובלי';
+$lang['Global activation'] = 'הפעלה גלובלית';
+$lang['HTTPS activated for administration pages only'] = 'HTTPS מופעל רק עבור דפי מנהלה';
+$lang['HTTPS activated for identification, register and profile pages only'] = 'HTTPS מופעל רק עבור דפי זיהוי, הרשמה ופרופיל';
+$lang['HTTPS available'] = 'HTTPS זמין';
+$lang['HTTPS unavailable'] = 'HTTPS אינו זמין';
+$lang['I have proceeded to the manual check'] = 'עברתי לבדיקה ידנית';
+$lang['Manual Verification'] = 'אימות ידני';
+$lang['Parial activation'] = 'הפעלה חלקית';
+$lang['Plugin could not automatically verify your host SSL/HTTPS compatibility. Please manually check that HTTPS navigation is available using the following button. Do NOT activate HTTPS if page within the popup can not be loaded successfully.'] = 'התוסף לא יכול לאמת אוטומטית את תאימות ה-SSL/HTTPS של המארח שלך. נא לבדוק ידנית שניווט HTTPS זמין באמצעות הכפתור הבא. אל תפעיל HTTPS אם הדף בתוך הקופץ לא נטען בהצלחה.';
+$lang['Your hosting server does not support HTTPS, a SSL certificate is required. Plugin activation is disabled.'] = 'שרת האחסון שלך לא תומך ב-HTTPS, נדרש תעודת SSL. הפעלת התוסף מנוטרלת.';
+$lang['Your hosting server does support HTTPS.'] = 'שרת האחסון שלך תומך ב-HTTPS.';
+$lang['fhp_about'] = 'אודות';
+$lang['fhp_activate_sts_comment'] = 'הפעלת אבטחת העברת מידע המחייבת (HSTS)<br/>אפשרות זו חייבת להיות מופעלת אם יש לך תעודת TLS חתומה. אין לה השפעה אם "לכפות HTTPS" מושבת<br/><br/><i>HSTS היא מנגנון אבטחת אינטרנט בו השרת מצהיר לדפדפן האינטרנט להתקשר אליו רק באמצעות חיבורי HTTPS מאובטחים. כאשר HSTS פעיל, שדה הכותרת "Strict-Transport-Security" מוצב בכותרת ה-HTTP, וכל קישור באתר מוחלף אוטומטית בקישור מאובטח.<br/></i>';
+$lang['fhp_activate_sts_comment_short'] = 'הפעלת אבטחת העברת מידע המחייבת (HSTS). לשימוש רק אם יש לך תעודת TLS חתומה';
+$lang['fhp_activate_sts_label'] = 'שימוש ב-STS';
+$lang['fhp_description'] = 'התוסף ממלא פער באבטחה ב-Piwigo. הוא מאפשר למנהלים לכפות את השימוש בחיבורים מאובטחים של SSL לגלישה ב-Piwigo (כאשר HTTPS זמין בשרת האחסון).<br>מומלץ ביותר להשתמש בחיבורים מאובטחים להגנת העברת משתמש וסיסמאות.';
+$lang['fhp_force_https_comment'] = 'הפעל אפשרות זו כדי לכפות על לקוחות להתחבר ל-Piwigo דרך חיבור מאובטח.';
+$lang['fhp_force_https_label'] = 'לכפות HTTPS';
+$lang['fhp_force_https_warning'] = '<b>אזהרה!</b> לפני הפעלת חיבור מאובטח, נא לוודא שהשרת שלך תומך ב-https. ניתן ללחוץ על הקישור הבא כדי לבדוק (טוען דף זה במצב https).';
+$lang['fhp_options_group'] = 'אפשרויות';
+$lang['fhp_security_group'] = 'הגדרות אבטחה';
+$lang['fhp_sts_maxage_comment'] = 'STS max-age בשניות';
+$lang['fhp_sts_maxage_label'] = 'STS max-age';
+$lang['fhp_submit'] = 'שלח';
+$lang['fhp_test_https_link'] = 'בדוק דף זה ב-https (חלון קופץ)';
+$lang['max-age must be set to a minimum of six months (15768000), but longer periods such as two years (63072000) are recommended'] = 'הזמן המרבי צריך להיות שווה לפחות לשישה חודשים (15768000), אך תקופה ארוכה יותר כמו שנתיים (63072000) מומלצת';
+$lang['seconds'] = 'שניות';
